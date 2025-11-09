f969710a34fc2bc5ab06bf2d2bc75f6159e70276
Chatrtham Chatramornrat <peenpeen21202@gmail.com>
2025-11-09 19:59:30 +0700

    Initial commit

---
+++ lib/chatApi.ts
@@ -0,0 +1,45 @@
+import { Client, ThreadState } from "@langchain/langgraph-sdk";
+import {
+  LangChainMessage,
+  LangGraphCommand,
+} from "@assistant-ui/react-langgraph";
+
+const createClient = () => {
+  const apiUrl = process.env["NEXT_PUBLIC_LANGGRAPH_API_URL"]!;
+  return new Client({
+    apiUrl,
+  });
+};
+
+export const createThread = async () => {
+  const client = createClient();
+  return client.threads.create();
+};
+
+export const getThreadState = async (
+  threadId: string,
+): Promise<ThreadState<{ messages: LangChainMessage[] }>> => {
+  const client = createClient();
+  return client.threads.getState(threadId);
+};
+
+export const sendMessage = async (params: {
+  threadId: string;
+  messages?: LangChainMessage[];
+  command?: LangGraphCommand | undefined;
+}) => {
+  const client = createClient();
+  return client.runs.stream(
+    params.threadId,
+    process.env["NEXT_PUBLIC_LANGGRAPH_ASSISTANT_ID"]!,
+    {
+      input: params.messages?.length
+        ? {
+            messages: params.messages,
+          }
+        : null,
+      command: params.command,
+      streamMode: ["messages", "updates"],
+    },
+  );
+};
